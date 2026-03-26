8c3cfb83a8f6bb9c2041b33808737b5dc21fbce8
pdmoura <sudwarriormoura@gmail.com>
2026-03-26 20:27:12 -0300

    refactor: remove legacy activities code

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,11 @@ router.get('/', (req, res) => {
     res.send(
       loggedIn
         ? `Logged in as ${req.user.displayName || req.user.username}. <a href="/auth/logout">Logout</a>`
-        : 'Welcome to the National Parks and Activities API. <a href="/auth/login">Login with GitHub</a>'
+        : 'Welcome to the National Parks API. <a href="/auth/login">Login with GitHub</a>'
     );
 });
 
 router.use('/auth', require('./auth'));
 router.use('/parks', require('./parks'));
-router.use('/activities', require('./activities'));
 
 module.exports = router;
