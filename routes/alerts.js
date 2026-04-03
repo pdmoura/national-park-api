f2f66364009d3d9c4e9143d31eca1fb46d981aca
abbigailf <jamesandabbigailnelson@gmail.com>
2026-04-03 12:56:52 -0600

    validator file added

--- routes/alerts.js
+++ routes/alerts.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const alertsController = require("../controllers/alerts");
 const { isAuthenticated } = require("../middleware/auth");
 const idParamRule = require("../validators/idParamValidator");
+const { createAlertRules } = require("../validators/alertsValidator");
 const validate = require("../middleware/validate");
 
 router.get("/", alertsController.getAllAlerts);
 router.get("/:id", idParamRule(), validate, alertsController.getAlertById);
-router.post("/", isAuthenticated, alertsController.createAlert);
-router.put("/:id", isAuthenticated, idParamRule(), validate, alertsController.updateAlert);
+router.post("/", isAuthenticated, createAlertRules(), validate, alertsController.createAlert);
+router.put("/:id", isAuthenticated, idParamRule(), createAlertRules(), validate, alertsController.updateAlert);
 router.delete("/:id", isAuthenticated, idParamRule(), validate, alertsController.deleteAlert);
 
 module.exports = router;
